df451d6169f145929342fbb8d49600ca834c0d7e
Sadan Mian <sadanmiyan007@gmail.com>
2023-07-30 23:14:01 +0530

    Dark Mode Added

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ arrow.forEach((event, index) => {
   let clickCounter = 0;
   event.addEventListener("click", () => {
     clickCounter++;
-    if (itemNumber - (4 + clickCounter) > 0) {
+    if (itemNumber - (4 + clickCounter) >= 0) {
       movieList[index].style.transform = `translateX(${
         movieList[index].computedStyleMap().get("transform")[0].x.value - 300
       }px)`;
@@ -15,5 +15,16 @@ arrow.forEach((event, index) => {
       clickCounter = 0;
     }
   });
-  console.log();
+});
+
+const ball = document.querySelector(".toggle-ball");
+const items = document.querySelectorAll(
+  ".container,.movie-list-title,.navbar-container,.sidebar,.left-menu-icon,.toggle"
+);
+
+ball.addEventListener("click", () => {
+  items.forEach((event) => {
+    event.classList.toggle("active");
+  });
+  ball.classList.toggle("active");
 });
